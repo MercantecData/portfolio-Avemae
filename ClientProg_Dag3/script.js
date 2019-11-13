bf98d2ca22aad4568f9be4c3fbc400ce6c4e1e8a
Sandra Jaworska <sandra.p.jaworska@gmail.com>
2019-11-13 15:09:22 +0100

    ClientProg_Dag3 - Extra files and small changes
    Started doing Javascript

---
+++ ClientProg_Dag3/script.js
@@ -0,0 +1,9 @@
+function ChangeText()
+{
+    document.getElementById("change-this-text").innerHTML = "~~ Hurray ~~";
+}
+
+function AddElement()
+{
+    document.getElementById("paragraph-with-javascript").innerHTML = "Paragraph Two added with JavaScript.";
+}
